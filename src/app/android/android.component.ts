745e0b9a11328af20a6156875e8844b1c5e062b4
Juan Esteban Torres Ibarguen <89664941+Black-software100@users.noreply.github.com>
2022-12-08 16:25:51 -0500

    Up

--- src/app/android/android.component.ts
+++ src/app/android/android.component.ts
@@ -8,16 +8,7 @@ import {  Component, ElementRef, ViewChild } from '@angular/core';
 export class ANDROIDComponent {
  
   
-  @ViewChild('test') Test: ElementRef;
 
-  change():void{
-    const prueba: any = document.querySelector('#test');
-    prueba.focus();
-    document.execCommand('selectAll')
-    document.execCommand('copy')
-    console.log(prueba.innerHTML);
-    prueba.style.color = 'red';
-  }
   
 }
 
